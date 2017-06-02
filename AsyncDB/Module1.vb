4f2aa03847eb13b878ea5e39c73cdb52b0f550f7
Jan Suchotzki <jan@lernmoment.de>
2017-06-02 12:27:38 +0200

    Wartet für 10 Sekunden

--- AsyncDB/Module1.vb
+++ AsyncDB/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Threading
 
 Module Module1
 
@@ -7,20 +8,31 @@ Module Module1
             Dim cmd As SqlCommand = New SqlCommand("select top 2 * from orders", conn)
 
             Dim result = OpenAndQueryAsync(conn, cmd).Result
+            Console.WriteLine("Result from async Connection: " + result)
 
-            Dim reader As SqlDataReader = cmd.ExecuteReader()
-            While reader.Read()
-                Console.WriteLine(String.Format("{0}", reader(0)))
-            End While
+            If conn.State = ConnectionState.Open Then
+                Dim reader As SqlDataReader = cmd.ExecuteReader()
+                While reader.Read()
+                    Console.WriteLine(String.Format("{0}", reader(0)))
+                End While
+            End If
         End Using
 
         Console.ReadLine()
     End Sub
 
-    Async Function OpenAndQueryAsync(conn As SqlConnection, cmd As SqlCommand) As Task(Of Integer)
-        Await conn.OpenAsync()
-        Await cmd.ExecuteNonQueryAsync()
-        Return 1
+    Async Function OpenAndQueryAsync(conn As SqlConnection, cmd As SqlCommand) As Task(Of String)
+        Dim cts As CancellationTokenSource = New CancellationTokenSource(10000)
+        Try
+            Await conn.OpenAsync(cts.Token)
+            Await cmd.ExecuteNonQueryAsync(cts.Token)
+        Catch tcex As TaskCanceledException
+            Return "Timeout"
+        Finally
+            cts.Dispose()
+        End Try
+
+        Return "Success"
     End Function
 
 End Module
